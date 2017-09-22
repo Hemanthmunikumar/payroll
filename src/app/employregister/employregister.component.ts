29755dd658840832ee51918ceadc1dc78b3e52c2
Hemanth <hemanthmunikumar@gmail.com>
2017-09-22 09:37:25 +0530

    added employee template

---
+++ src/app/employregister/employregister.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employregister',
+  templateUrl: './employregister.component.html',
+  styleUrls: ['./employregister.component.css']
+})
+export class EmployregisterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
